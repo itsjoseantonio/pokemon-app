e7a03616d2a422929717ed55ab17e3f6a03e67de
Jose Antonio Valga <jos.valga@gmail.com>
2025-11-01 10:07:23 -0500

    test: Testing setup

---
+++ client/vitest.config.ts
@@ -0,0 +1,15 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./src/test/setup.ts'],
+    globals: true,
+    css: false, // Speeds up tests by skipping CSS processing
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+  },
+});
